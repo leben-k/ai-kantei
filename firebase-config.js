db55ca8ce43befe941155f17f8646f2334dd638c
leben-k <your.senyo@gmail.com>
2026-09-11 10:18:40 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -20,7 +20,14 @@
 // 実際のアクセス制御は Firestore / Storage のセキュリティルールと Authentication（ログイン）で行います。
 
 export const firebaseConfig = {
-  apiKey: "REPLACE_ME",
+  apiKey: "const firebaseConfig = {
+  apiKey: "AIzaSyAZut9s6hsKagjQhGLa2axBiKa8jmtgz54",
+  authDomain: "tasuku-kanri-2380a.firebaseapp.com",
+  projectId: "tasuku-kanri-2380a",
+  storageBucket: "tasuku-kanri-2380a.firebasestorage.app",
+  messagingSenderId: "291068614905",
+  appId: "1:291068614905:web:73402f4b4b9cb7e707ea28"
+};",
   authDomain: "tasuku-kanri-2380a.firebaseapp.com",
   projectId: "tasuku-kanri-2380a",
   storageBucket: "tasuku-kanri-2380a.appspot.com",
